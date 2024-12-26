19970c515d670981176634695e82cb726fe980af
brotigen23 <brotigen23@gmail.com>
2024-12-26 19:18:43 +0300

    fix: check order status

--- internal/handler/userHandler.go
+++ internal/handler/userHandler.go
@@ -144,53 +144,30 @@ func (h *userHandler) SaveOrder(rw http.ResponseWriter, r *http.Request) {
 	log.Println("order", order, "registered by", userLogin)
 	// create goroutine to check order status
 	for {
+		time.Sleep(time.Second)
 		resp, err := http.Get(h.Config.AccrualSystemAddress + "/api/orders/" + order)
 		if err != nil {
 			log.Println(err.Error())
 			return
 		}
-		defer resp.Body.Close()
-		switch resp.StatusCode {
-		case http.StatusNoContent:
-			for i := 0; i < 10; i++ {
-				time.Sleep(time.Second)
-				resp, err = http.Get(h.Config.AccrualSystemAddress + "/api/orders/" + order)
-				if err != nil {
-					log.Println(err.Error())
-					return
-				}
-				if resp.StatusCode != http.StatusOK {
-					continue
-				} else {
-					break
-				}
-			}
-			if resp.StatusCode != http.StatusOK {
-				log.Println("accrual error: order is not registered")
-				return
-			}
-		}
 		if resp.StatusCode != http.StatusOK {
-			return
+			continue
 		}
 		o, err := utils.UnmarhallOrder(resp.Body)
 		if err != nil {
 			log.Println(err.Error())
 			return
 		}
+		defer resp.Body.Close()
 		log.Println(o)
 		switch o.Status {
 		case "PROCESSED":
 			log.Println("PROCESSED")
 			h.userService.UpdateUserBalance(userLogin.Value, o.Accrual)
 			return
-		case "INVALID":
-			return
-		case "PROCCESSING", "REGISTERED":
 		default:
 			return
 		}
-		time.Sleep(time.Second)
 	}
 }
 
