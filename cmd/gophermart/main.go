84ac7cf28622aa2c6491b5e82d82547fc9ce0019
brotigen23 <brotigen23@gmail.com>
2024-12-26 14:37:35 +0300

    oops

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/brotigen23/gopherMart/internal/utils"
-)
+import "github.com/brotigen23/gopherMart/internal/app"
 
 func main() {
-	//app.Run(":8080")
-	log.Println(utils.IsOrderCorrect("34137456132"))
+	app.Run(":8080")
 }
