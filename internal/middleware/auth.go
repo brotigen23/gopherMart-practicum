6ff5aabcaaafa23e816877743fdfc694078d6c10
brotigen23 <brotigen23@gmail.com>
2025-01-09 22:40:04 +0300

    feat: add ci, mocks

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -9,25 +9,6 @@ import (
 
 var JWTSecretKey = "secret"
 
-func auth(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("token")
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(rw, err.Error(), http.StatusUnauthorized)
-			return
-		}
-		// * Если ошибка то токен непонятный, иначе все норм
-		user, err := utils.GetUserLoginFromJWT(cookie.Value, JWTSecretKey)
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(rw, err.Error(), http.StatusUnauthorized)
-			return
-		}
-		r.AddCookie(&http.Cookie{Name: "userLogin", Value: user})
-	})
-}
-
 func Auth(secretKey string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,17 +1,1 @@
 package utils
-
-import (
-	"testing"
-)
-
-func TestUnmarhallUser(t *testing.T) {
-
-}
-
-func TestBuildJWT(t *testing.T) {
-
-}
-
-func TestGetUserLoginFromJWT(t *testing.T) {
-
-}
